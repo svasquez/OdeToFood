b9856a555edf1d9b772ba967d2af430ce9d2089f
Smill Vásquez <smillalexander@gmail.com>
2018-07-08 23:35:45 -0400

    Add project files.

---
+++ OdeToFood/ISaludo.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface ISaludo
+    {
+        string GetMessageOfDay();
+    }
+
+    public class Saludo : ISaludo
+    {
+        private readonly IConfiguration _configuration;
+
+        public Saludo(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetMessageOfDay() => _configuration["Saludos"];
+    }
+}
