2780928853e2f3667696bc26751c5a9a099912a6
Smill Vásquez <smillalexander@gmail.com>
2018-07-10 22:57:29 -0400

    --adding custom middleware example

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace OdeToFood
 {
@@ -20,12 +21,31 @@ namespace OdeToFood
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ISaludo saludo)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ISaludo saludo , ILogger<Startup> logger)
         {
             //if (env.IsDevelopment())
             //{
             //    app.UseDeveloperExceptionPage();
             //}
+
+            app.Use(next => 
+            {
+                return async context =>
+                {
+                    logger.LogInformation("Request Incoming!!");
+                    if(context.Request.Path.StartsWithSegments("/mym"))
+                    {
+                        await context.Response.WriteAsync("Activo");
+                        logger.LogInformation("Request Handled");
+                    }
+
+                    else
+                    {
+                        await next(context);
+                        logger.LogInformation("Response Outgoing");
+                    }
+                };
+            });
 
             app.UseWelcomePage(new WelcomePageOptions
             {
